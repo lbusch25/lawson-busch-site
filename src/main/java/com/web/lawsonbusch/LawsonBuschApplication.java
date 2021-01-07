8ec2dda22e6cd04b752f2b943134d2bee246df42
lbusch25 <lbusch@macalester.edu>
2021-01-06 19:32:53 -0700

    added route handling for angular routes

--- src/main/java/com/web/lawsonbusch/LawsonBuschApplication.java
+++ src/main/java/com/web/lawsonbusch/LawsonBuschApplication.java
@@ -2,12 +2,28 @@ package com.web.lawsonbusch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @SpringBootApplication
-public class LawsonBuschApplication {
+public class LawsonBuschApplication implements ErrorController {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LawsonBuschApplication.class, args);
 	}
 
+	private static final String ERROR_PATH = "/error";
+	private static final String DEFAULT_FORWARD = "forward:/index.html";
+
+	@RequestMapping(value = ERROR_PATH)
+	public String error() {
+		return DEFAULT_FORWARD;
+	}
+
+	@Override
+	public String getErrorPath() {
+		return ERROR_PATH;
+	}
 }
